7b41287d246f211871cd7cd182cf32f8aa4fb8f9
tsatkgr <31375242+tsatkgr@users.noreply.github.com>
2017-12-07 17:55:43 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
   var nav = $("nav");
   //Nav Filler
   //navfiller.height(nav.first().height());
+  
+  	
+	  $(function(){ 
+
+    $('a.read_more').click(function(event){ 
+
+        event.preventDefault();
+        $(this).parents('.item').find('.more_text').show(); 
+
+    });
+
+});
 });
